3e17bcf7f736b4a8171bc199df901febfd06992e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-03 11:59:02 -1100

    Add feature branch

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import Helpers from './modules/Helpers.js';
+import Interactives from './modules/Interactives.js';
+import Drag from './modules/Drag.js';
+import './style.css';
+
+const task = new Helpers();
+const utils = new Interactives();
+
+const list = document.querySelector('.list');
+const form = document.querySelector('form');
+const inputField = document.querySelector('.list_input');
+const clearBtn = document.querySelector('.clear_completed');
+
+function clearInputs() {
+  inputField.value = '';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  task.addTask(task.taskList.length + 1, document.querySelector('.list_input').value);
+  utils.checkCompleted(task.taskList);
+  clearInputs();
+});
+
+list.addEventListener('click', (e) => {
+  const target = e.target.closest('.fa-ellipsis-vertical');
+  const trash = e.target.closest('.fa-trash-can');
+  const description = e.target.closest('.description');
+
+  if (target) {
+    target.nextElementSibling.classList.toggle('show');
+    target.style.display = 'none';
+  }
+
+  if (trash) {
+    task.removeTask(Number(trash.id));
+    utils.checkCompleted(task.taskList);
+  }
+
+  if (description && !description.classList.contains('blur_text')) {
+    task.editTask(description);
+  }
+});
+
+list.addEventListener('focusout', (e) => {
+  const description = e.target.closest('.description');
+  if (description) {
+    task.displayEditedTask(description, Number(description.dataset.id));
+    task.displayList();
+    Drag.runDrag();
+  }
+});
+
+list.addEventListener('change', (e) => {
+  const checkBox = e.target.closest('.check_item');
+  if (checkBox) {
+    utils.toggleCompleted(Number(checkBox.id), task.taskList, checkBox);
+    checkBox.nextElementSibling.classList.toggle('blur_text');
+    task.sortTasks();
+    task.updateStorage();
+  }
+});
+
+clearBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  task.taskList = utils.clearCompleted(task.taskList);
+  task.sortTasks();
+  task.updateStorage();
+  task.displayList();
+  Drag.runDrag();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  utils.checkCompleted(task.taskList);
+  Drag.runDrag();
+});
+
+window.onload = task.displayList();
---
+++ src/modules/Helpers.js
@@ -0,0 +1,84 @@
+/* eslint-disable class-methods-use-this */
+import Task from './Task.js';
+import Drag from './Drag.js';
+
+export default class Helpers {
+  constructor() {
+    this.taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
+
+  addTask(index, value) {
+    if (value === '') return;
+    const newTask = new Task(index, value);
+    this.addToTaskList(newTask);
+    this.sortTasks();
+    this.updateStorage();
+    this.displayList();
+    Drag.runDrag();
+  }
+
+  addToTaskList(task) {
+    this.taskList.push(task);
+  }
+
+  displayList() {
+    document.querySelector('.list').style.display = 'block';
+
+    if (this.taskList.length === 0) {
+      document.querySelector('.list').style.display = 'none';
+    }
+    document.querySelector('.list').innerHTML = `
+    ${this.setListItems(this.taskList)}
+    `;
+  }
+
+  removeTask(i) {
+    this.taskList = this.taskList.filter((item) => item.index !== i);
+    this.taskList.forEach((item, i) => {
+      item.index = i + 1;
+    });
+    this.sortTasks();
+    this.updateStorage();
+    this.displayList();
+    Drag.runDrag();
+  }
+
+  setListItems(arr) {
+    let listItems = '';
+    for (let i = 0; i < arr.length; i += 1) {
+      listItems += `
+      <li class='list_item' data-id='${arr[i].index}' draggable='true'>
+      <div>
+        <input type='checkbox' class='check_item' id=${arr[i].index}>
+        <span class='description' data-id=${arr[i].index}>${arr[i].description}</span>
+      </div>
+      <i class='fa-solid fa-ellipsis-vertical'></i>
+      <i class='fa-solid fa-trash-can' id=${arr[i].index}></i>
+      </li>
+      `;
+    }
+    return listItems;
+  }
+
+  sortTasks() {
+    this.taskList.sort((a, b) => a.index - b.index);
+  }
+
+  editTask(desc) {
+    desc.setAttribute('contenteditable', 'true');
+    desc.focus();
+  }
+
+  displayEditedTask(elem, index) {
+    elem.setAttribute('contenteditable', 'false');
+    const objIndex = this.taskList.findIndex((obj) => obj.index === index);
+    this.taskList[objIndex].description = elem.innerText;
+    Drag.runDrag();
+    this.sortTasks();
+    this.updateStorage();
+  }
+
+  updateStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.taskList));
+  }
+}
